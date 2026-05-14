1f5ff6e780b2c2590597fc0c59a819b2c38e03e0
MdShariyar56 <shahriyarkobir2005@gmail.com>
2026-05-14 12:02:06 +0600

    Setup a react project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Contact from './Pages/Contact';
+import Footer from './Components/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Contact></Contact>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default App;
